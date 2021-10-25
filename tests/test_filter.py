f72b91fb513b1759ca054bd79611acb23e1b78d7
Diane Trout <diane@ghic.org>
2021-10-25 11:39:50 -0700

    Test that the filtering works.
    Apparently just printing the result is not actually a test.

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -40,5 +40,49 @@ class TestFilter(TestCase):
         raw_fake_barcodes = read_barcode_lineno_map(
             StringIO(os.linesep.join(generate_fake_barcodes(4)))
         )
+
+        # The generate fake barcodes never gets to C and only includes the following
+        # G lines.
+        # 'ACCC': 20,
+        # 'GGGG': 21,
+        # 'GGGT': 22,
+        # 'GGGC': 23,
+        # 'GGTT': 24,
+        # 'GGTC': 25,
+        # 'GGCC': 26,
+        # 'GTTT': 27,
+        # 'GTTC': 28,
+        # 'GTCC': 29,
+        # 'GCCC': 30,
+        # 'TTTT': 31,
+        # So it's lines 21-30 that get deleted and need to be remapped.
+
         mapping = compute_raw_to_filtered_map(filtered_fake_barcodes, raw_fake_barcodes)
-        print(mapping)
+        expected_mapping = {
+            1: 1,
+            2: 2,
+            3: 3,
+            4: 4,
+            5: 5,
+            6: 6,
+            7: 7,
+            8: 8,
+            9: 9,
+            10: 10,
+            11: 11,
+            12: 12,
+            13: 13,
+            14: 14,
+            15: 15,
+            16: 16,
+            17: 17,
+            18: 18,
+            19: 19,
+            20: 20,
+            31: 21,
+            32: 22,
+            33: 23,
+            34: 24,
+        }
+
+        self.assertEqual(mapping, expected_mapping)
